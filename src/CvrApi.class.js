793084c0e05490496720ebb4df8c2933fd635c16
kevin <kevinfrom@live.dk>
2020-05-09 21:27:08 +0200

    Removed browser support comment from docblock

--- src/CvrApi.class.js
+++ src/CvrApi.class.js
@@ -2,9 +2,6 @@
  * CvrApi.class.js
  * @author Kevin From <kevinfrom@live.dk>
  * @website https://kevinfrom.dk
- *
- * Estimated browser support:
- * https://caniuse.com/#search=javascript%20class
  */
 
 class CvrApi {
