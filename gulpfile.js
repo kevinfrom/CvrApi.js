a21c92a16ae05110781cf658d3d82c7351056058
kevin <kevinfrom@live.dk>
2020-05-09 21:40:24 +0200

    v2.1 - created minified versions for all versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const gulp = require('gulp'),
     babel = require('gulp-babel'),
     rename = require('gulp-rename'),
-    uglify = require('gulp-uglify');
+    terser = require('gulp-terser'),
+    strip = require('gulp-strip-comments');
 
-gulp.task('modern', () => {
+gulp.task('es6', () => {
     return gulp.src('src/CvrApi.class.js')
         .pipe(babel({
             presets: [
@@ -23,24 +24,66 @@ gulp.task('modern', () => {
         .pipe(gulp.dest('dist'))
 });
 
-gulp.task('old', () => {
+gulp.task('es6-min', () => {
     return gulp.src('src/CvrApi.class.js')
-    .pipe(babel({
-        presets: [
-            [
-                '@babel/env',
-                {
-                    targets: [
-                        'ie >= 11',
-                        'safari >= 9'
-                    ]
-                }
+        .pipe(babel({
+            presets: [
+                [
+                    '@babel/env',
+                    {
+                        targets: [
+                            'last 2 versions',
+                            'not dead',
+                            'not < 2%',
+                        ],
+                    }
+                ]
             ]
-        ]
-    }))
-    .pipe(uglify())
-    .pipe(rename('CvrApi.min.js'))
-    .pipe(gulp.dest('dist'))
+        }))
+        .pipe(rename('CvrApi.es6.min.js'))
+        .pipe(strip())
+        .pipe(terser())
+        .pipe(gulp.dest('dist'))
 });
 
-exports.default = gulp.parallel('modern', 'old');
+gulp.task('old', () => {
+    return gulp.src('src/CvrApi.class.js')
+        .pipe(babel({
+            presets: [
+                [
+                    '@babel/env',
+                    {
+                        targets: [
+                            'ie >= 11',
+                            'safari >= 9'
+                        ]
+                    }
+                ]
+            ]
+        }))
+        .pipe(rename('CvrApi.js'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('old-min', () => {
+    return gulp.src('src/CvrApi.class.js')
+        .pipe(babel({
+            presets: [
+                [
+                    '@babel/env',
+                    {
+                        targets: [
+                            'ie >= 11',
+                            'safari >= 9'
+                        ]
+                    }
+                ]
+            ]
+        }))
+        .pipe(strip())
+        .pipe(terser())
+        .pipe(rename('CvrApi.min.js'))
+        .pipe(gulp.dest('dist'))
+});
+
+exports.default = gulp.parallel('es6', 'old', 'es6-min', 'old-min');
